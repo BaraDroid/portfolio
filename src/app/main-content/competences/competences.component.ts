6ec49ddcafd9b406f15d2e4e6c701404d48054ba
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-19 07:52:33 +0100

    my work section basic styling

--- src/app/main-content/competences/competences.component.ts
+++ src/app/main-content/competences/competences.component.ts
@@ -7,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrl: './competences.component.scss'
 })
 export class CompetencesComponent {
-
+icons: {
+  path: string;
+  description: string;
+} [] = [
+  {
+    path: './assets/frontend_icons/html.svg',
+    description: 'HTML'
+  },
+  {
+    path: './assets/frontend_icons/css.svg',
+    description: 'CSS'
+  },
+  {
+    path: './assets/frontend_icons/js.svg',
+    description: 'JavaScript'
+  },
+  {
+    path: './assets/frontend_icons/ts.svg',
+    description: 'TypeScript'
+  },
+  {
+    path: './assets/frontend_icons/angular.svg',
+    description: 'Angular'
+  },
+  {
+    path: './assets/frontend_icons/firebase.svg',
+    description: 'Firebase'
+  },
+  {
+    path: './assets/frontend_icons/git.svg',
+    description: 'Git'
+  },
+  {
+    path: './assets/frontend_icons/rest_api.svg',
+    description: 'Rest-Api'
+  },
+  {
+    path: './assets/frontend_icons/scrum.svg',
+    description: 'Scrum'
+  },
+  {
+    path: './assets/frontend_icons/material_design.svg',
+    description: 'Material design'
+  }
+];
 }
