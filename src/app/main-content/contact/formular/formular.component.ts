c3736933da0fb3719abafc5675f0f1bc0742a175
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-20 09:36:20 +0100

    interface ContactForm added

--- src/app/main-content/contact/formular/formular.component.ts
+++ src/app/main-content/contact/formular/formular.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
 
+interface ContactForm {
+  name: string;
+  email: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-formular',
   imports: [],
   templateUrl: './formular.component.html',
   styleUrl: './formular.component.scss'
 })
+
 export class FormularComponent {
+  
+myForm: ContactForm = {
+  name: "",
+  email: "",
+  message: "",
+}
+
+sendEmail() {
+  console.log(this.myForm);
+}
 
 }
