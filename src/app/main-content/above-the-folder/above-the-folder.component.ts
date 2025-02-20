52fd55d371aada0508011c173918467910c76551
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-20 05:14:48 +0100

    contact section styled

--- src/app/main-content/above-the-folder/above-the-folder.component.ts
+++ src/app/main-content/above-the-folder/above-the-folder.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MainNavbarComponent } from '../../shared/main-navbar/main-navbar.component';
 
 @Component({
   selector: 'app-above-the-folder',
-  imports: [],
+  imports: [MainNavbarComponent],
   templateUrl: './above-the-folder.component.html',
   styleUrl: './above-the-folder.component.scss'
 })
--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FormularComponent } from './formular/formular.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormularComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
