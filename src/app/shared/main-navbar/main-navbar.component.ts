ed1ba66502e3b62c0a1fa33f99b8c09b2c141ef8
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-02-24 16:43:11 +0100

    svg hover effect

--- src/app/shared/main-navbar/main-navbar.component.ts
+++ src/app/shared/main-navbar/main-navbar.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main-navbar.component.html',
   styleUrl: './main-navbar.component.scss'
 })
 export class MainNavbarComponent {
+
+  githubIconHover: boolean = false;
+  linkedinIconHover:boolean = false;
+  mailIconHover: boolean = false;
+
 
 }
