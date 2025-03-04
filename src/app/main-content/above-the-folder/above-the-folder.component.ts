36f811cf4190d546c2bec32d61805d3e67cbcc48
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-03-04 10:19:20 +0100

    translation to english

--- src/app/main-content/above-the-folder/above-the-folder.component.ts
+++ src/app/main-content/above-the-folder/above-the-folder.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MainNavbarComponent } from '../../shared/main-navbar/main-navbar.component';
 import { CommonModule } from '@angular/common';
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-above-the-folder',
-  imports: [MainNavbarComponent, CommonModule, TranslatePipe],
+  imports: [MainNavbarComponent, CommonModule, TranslatePipe, TranslateDirective],
   templateUrl: './above-the-folder.component.html',
   styleUrl: './above-the-folder.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -19,5 +19,7 @@ export class MainContentComponent {
 
   constructor(private translate: TranslateService) {}
 
+  
+
 
 }
