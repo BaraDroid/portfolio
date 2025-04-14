b05ebd49006c98223e4cec59d86e9516c201bcbe
BaraDroid <lambeinovab@seznam.cz>
2025-04-14 14:58:47 +0200

    rendering projects directly from my little database in work.component.ts

--- src/app/main-content/work/work.component.ts
+++ src/app/main-content/work/work.component.ts
@@ -15,6 +15,7 @@ export class WorkComponent {
   isWhite: boolean = false;
   indexOfThisSection: number = 3;
   showContribution: boolean = false;
+  currentIndex: number = 0;
 
   projects =[
     {
@@ -24,8 +25,32 @@ export class WorkComponent {
       iconSrc: './assets/emojis/statistic.svg',
       gitHubUrl: 'https://github.com/BaraDroid/pokedex',
       liveUrl: 'https://pokedex.barbora-lambeinova.de/'
-    }
+    },
+    {
+      name: 'Join',
+      imgSrc: '././assets/screenshots/join.png',
+      technologies: 'Angular | Firebase | TypeScript | SCSS | HTML',
+      iconSrc: './assets/emojis/join.svg',
+      gitHubUrl: 'https://i.kym-cdn.com/photos/images/original/000/310/238/0e5.png',
+      liveUrl: 'https://i.kym-cdn.com/photos/images/original/000/310/238/0e5.png'
+    },
+    {
+      name: 'El Pollo Loco',
+      imgSrc: '././assets/screenshots/pollo_loco.png',
+      technologies: 'Objektorientierung | JavaScript | CSS | HTML',
+      iconSrc: './assets/emojis/pollo_loco.svg',
+      gitHubUrl: 'https://github.com/BaraDroid/El-pollo-loco',
+      liveUrl: 'https://i.kym-cdn.com/photos/images/original/000/310/238/0e5.png'
+    },
   ]
+
+  showNextProject() {
+    this.currentIndex++;
+  }
+
+  showPreviousProject() {
+    this.currentIndex--;
+  }
     
 
   //menu.service
