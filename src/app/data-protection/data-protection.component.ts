e1ab87a459229ac9770ac69fa06f6ce811a034fd
BaraDroid <lambeinovab@seznam.cz>
2025-04-20 15:07:27 +0200

    imprint and privacy policy translated into english

--- src/app/data-protection/data-protection.component.ts
+++ src/app/data-protection/data-protection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainNavbarComponent } from '../shared/main-navbar/main-navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-data-protection',
-  imports: [MainNavbarComponent, FooterComponent],
+  imports: [MainNavbarComponent, FooterComponent, TranslatePipe],
   templateUrl: './data-protection.component.html',
   styleUrl: './data-protection.component.scss'
 })
