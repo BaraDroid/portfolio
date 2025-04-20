106d40cded4bc34334ee6a67ad5f5b7ee56e4496
BaraDroid <lambeinovab@seznam.cz>
2025-04-20 13:55:27 +0200

    imprint translated into english

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainNavbarComponent } from '../shared/main-navbar/main-navbar.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-impressum',
-  imports: [MainNavbarComponent, FooterComponent],
+  imports: [MainNavbarComponent, FooterComponent, TranslatePipe],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
